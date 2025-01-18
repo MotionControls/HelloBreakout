5b68511606d674f8bcb841ccab3f683b2af7bdd6
MotionControls <bprogers43@tntech.edu>
2025-01-18 05:45:48 -0600

    Update types.h
    Added Vec2sfx16 and Vec2ufx32 data types.

--- types.h
+++ types.h
@@ -9,4 +9,16 @@ typedef struct{
 	int x,y,w,h;
 }Vec4i;
 
+// These already have equivalents in libgte,
+// but for the sake of "clean" I'm making my own here.
+typedef struct{	// Ideal for normalized vectors.
+	short x,y;	// 64 >> 6 = 1
+				// 32 >> 6 = 0.5
+}Vec2sfx16;
+
+typedef struct{			// Ideal for positions.
+	unsigned long x,y;	// 4096 >> 12 = 1
+						// 2048 >> 12 = 0.5
+}Vec2ufx32;
+
 #endif
